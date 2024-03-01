9ecf08f62d89de1681f6c763a1242e44bca7b306
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-01 12:36:02 +0100

    add files

---
+++ script.js
@@ -0,0 +1,59 @@
+const gridContainer = document.querySelector('.my_container');
+ 
+
+//  const btn_genera = document.getElementById('btn_genera');
+
+// btn_genera.addEventListener('click' reset);
+
+
+reset();
+
+for(let i = 1; i <= 100; i++){
+  const square = getsquare(i);
+  
+  gridContainer.append(square);
+}
+
+
+
+
+
+
+
+
+
+
+// function
+
+function reset(){
+  gridContainer.innerHTML = '';
+}
+
+function getsquare(numero){
+  const sq = document.createElement('div');
+  sq.className = 'square';
+  sq._sqId = numero ;
+   //(this in questo caso è il mio quadrato è il mio elemento che clicco ricrda che sta dentro a click! NB)
+  sq.addEventListener('click', function(){
+    console.log(this._sqId)
+    
+    if(this.innerHTML === ''){
+      this.innerHTML = this._sqId;
+    }else{
+      this.innerHTML = '';
+    }
+
+    this.classList.toggle('clicked');
+    this.classList.toggle(getOddEventClass(numero))
+  })
+
+
+  return sq;
+}
+
+function getOddEventClass(n){
+  if(n % 2 === 0){
+    return'event'
+  }
+  return 'odd'
+}
